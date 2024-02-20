e5a9d4b428cc72eb4a7f5db61c03b3f3b10298db
Igor Lashyn <ilashyn@gmail.com>
2024-02-20 14:18:43 +0200

    leet code 14 longes common prefix

--- src/clojure_learning/leetcode.clj
+++ src/clojure_learning/leetcode.clj
@@ -90,20 +90,18 @@
    (fn [result string]
      (and result (cs/starts-with? string prefix))) true strings))
 
-(defn longest_common_prefix [ first & strings ]
+(defn longest_common_prefix [ [first & strings] ]
   (let [splited (vec first) count (count splited)]
-    (println strings)
-    (println splited)
     (loop [acc [] x 0]
-      (let  [next (conj acc (get splited (+ x 1)))]
+      (let  [next (conj acc (get splited x))]
         (if (>= x count)
           (cs/join "" acc)
           (if (has_prefix? (cs/join "" next) strings)
             (recur next (+ x 1))
             (recur acc (+ x count))))))))
 
-(longest_common_prefix [ "flower" "flow" "flight"])
-;; 'fl'
+(longest_common_prefix [ "flower" "flow" "floight"])
+;; 'flo'
 
 (longest_common_prefix [ "dog" "racecar" "car"])
 ;; '' 
