5d84d45500f4d30472e15bd6944ed3e4c610c2c4
Igor Lashyn <ilashyn@gmail.com>
2024-02-20 11:51:15 +0200

    leet code "two sum"

--- src/clojure_learning/leetcode.clj
+++ src/clojure_learning/leetcode.clj
@@ -17,6 +17,24 @@
 ;; => #{8}
 
 
+(defn two_sum [numbers target]
+  (:result (reduce-kv
+   (fn [m k v]
+     (let [diff (- target v)]
+        (let [{:keys [result swap]} m]
+            (if (> 2 (count result))
+                (if(some #(= diff %) (keys swap))
+                    {:swap swap :result [(get swap diff) k]}
+                    {:swap (assoc swap v k) :result result})
+                m)))) {:swap {} :result []} numbers)))
 
+(two_sum [2, 7, 11, 15] 9)
+;; => [0 1]
+
+(two_sum [3, 2, 4] 6)
+;; => [1 2]
+
+(two_sum [3, 3] 6)
+;; => [0 1]
 
   
