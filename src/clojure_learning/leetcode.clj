53ba38f91b3dbcc4ef85128b51fbcf2338e4c91e
Igor Lashyn <ilashyn@gmail.com>
2024-02-20 13:15:34 +0200

    remove debug

--- src/clojure_learning/leetcode.clj
+++ src/clojure_learning/leetcode.clj
@@ -50,28 +50,25 @@
 
 (def roman_to_int_map 
   {\I 1,
-                       "IV" 4,
-                       \V 5,
-                       "IX" 9,
-                       \X 10,
-                       "XL" 40
-                       \L 50,
-                       "XC" 90
-                       \C 100,
-                       "CD" 400
-                       \D 500,
-                       "CM" 900
-                       \M 1000})
+    "IV" 4,
+    \V 5,
+    "IX" 9,
+    \X 10,
+    "XL" 40
+    \L 50,
+    "XC" 90
+    \C 100,
+    "CD" 400
+    \D 500,
+    "CM" 900
+    \M 1000})
+
 (defn split_roman [roman]
   (let [splited (vec roman) count (count splited) ]
     (loop [x 0 acc []]
       (if (>= x count)
         acc 
         (let [current (nth splited x) with_next (cs/join "" [current (nth splited (+ x 1) \\)])]
-        (println current)
-          (println with_next)
-          (println acc)
-          (println x)
             (if (some #(= with_next %) (keys roman_to_int_map))
                 (recur (+ x 2) (conj acc with_next))
                 (recur (+ x 1) (conj acc current))))))))
