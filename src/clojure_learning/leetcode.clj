a75851ee3ae82ac47319b9a13722aae27dbfcd53
Igor Lashyn <ilashyn@gmail.com>
2024-02-20 13:13:02 +0200

    leet code 13 roman to int

--- src/clojure_learning/leetcode.clj
+++ src/clojure_learning/leetcode.clj
@@ -46,3 +46,44 @@
 ;; => false
 
 (palindrom_number 121)
+;; => true
+
+(def roman_to_int_map 
+  {\I 1,
+                       "IV" 4,
+                       \V 5,
+                       "IX" 9,
+                       \X 10,
+                       "XL" 40
+                       \L 50,
+                       "XC" 90
+                       \C 100,
+                       "CD" 400
+                       \D 500,
+                       "CM" 900
+                       \M 1000})
+(defn split_roman [roman]
+  (let [splited (vec roman) count (count splited) ]
+    (loop [x 0 acc []]
+      (if (>= x count)
+        acc 
+        (let [current (nth splited x) with_next (cs/join "" [current (nth splited (+ x 1) \\)])]
+        (println current)
+          (println with_next)
+          (println acc)
+          (println x)
+            (if (some #(= with_next %) (keys roman_to_int_map))
+                (recur (+ x 2) (conj acc with_next))
+                (recur (+ x 1) (conj acc current))))))))
+
+(defn roman_to_int [roman]
+  (reduce 
+   (fn [acc r] (+ acc (get roman_to_int_map r)))
+   0 (split_roman roman)))
+
+(roman_to_int "III")
+;; 3
+(roman_to_int "LVIII")
+;; 58
+(roman_to_int "MCMXCIV")
+;;1994
